43520bdfa6439dcc85f5eeef8bf9bd140a49ead1
JujuRC <justinebakundukize@gmail.com>
2026-08-26 23:28:21 +0200

    Adding comments
    Important comments were missing.

--- ICE_TASK_2/src/main/java/com/mycompany/ice_task_2/ICE_TASK_2.java
+++ ICE_TASK_2/src/main/java/com/mycompany/ice_task_2/ICE_TASK_2.java
@@ -10,25 +10,27 @@ import java.util.Scanner;
  */
 public class ICE_TASK_2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Declaration of variables 
         double Num1, Num2 , Sum , Multiple, Difference, Quotient, Average;
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Enter your first number.");
+        System.out.print("Enter your first number."); //prompting user for first number
         Num1 = scanner.nextDouble();
         
-        System.out.print("Enter your second number.");
+        System.out.print("Enter your second number.");//prompting user for second number
         Num2 = scanner.nextDouble();
         
+        //Calculations
         Sum = add(Num1, Num2);
         Difference = subtract(Num1, Num2);
         Multiple = multiple(Num1, Num2);
         Quotient = divide(Num1, Num2);
         Average = average(Num1,Num2);
         if (Num2 != 0) {
-            System.out.println("Division:       Cannot divide by zero.");
+            System.out.println("Quotient:" + Num1 + "Cannot divide by zero.");
         }
         
+        //Printing out the calculations 
         System.out.println("Calculations using" + Num1 + "and" + Num2);
         System.out.println("Sum:" + Sum);
         System.out.println("Difference:" + Sum);
@@ -38,7 +40,7 @@ public class ICE_TASK_2 {
        
         scanner.close();
     }
-    
+    //Declaration of methonds
     public static double add(double a, double b) {
         return a + b;
     }
